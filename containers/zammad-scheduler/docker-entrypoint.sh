4c998f88d6ba901228b4e24563725b407df12f0a
André Bauer <abauer@magix.net>
2017-01-12 09:18:45 +0100

    fix schedulter & websocket process

--- containers/zammad-scheduler/docker-entrypoint.sh
+++ containers/zammad-scheduler/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 if [ "$1" = 'zammad-scheduler' ]; then
 
     cd ${ZAMMAD_DIR}
-    bundle exec script/scheduler.rb start &
+    bundle exec script/scheduler.rb start
 
 fi
--- containers/zammad-websocket/docker-entrypoint.sh
+++ containers/zammad-websocket/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 if [ "$1" = 'zammad-websocket' ]; then
 
     cd ${ZAMMAD_DIR}
-    bundle exec script/websocket-server.rb -b 0.0.0.0 start &
+    bundle exec script/websocket-server.rb -b 0.0.0.0 start
 
 fi
