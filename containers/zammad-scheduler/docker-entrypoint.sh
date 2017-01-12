543c8e4580677d4c1dd85b5896ce5403833cab23
André Bauer <abauer@magix.net>
2017-01-12 10:34:01 +0100

    changes

--- containers/zammad-scheduler/docker-entrypoint.sh
+++ containers/zammad-scheduler/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$1" = 'zammad-scheduler' ]; then
 
     # delete pid
-    find ${ZAMMAD_DIR}/tmp/pids -iname scheduler.pid -exec rm {} \;
+#    find ${ZAMMAD_DIR}/tmp/pids -iname scheduler.pid -exec rm {} \;
 
     # start scheduler
     cd ${ZAMMAD_DIR}
